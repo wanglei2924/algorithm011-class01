edf2a2b69f017b9bd66520068e78c4d3533d0403
wanglei2924 <wanglei2924@hotmail.com>
2020-07-19 23:37:53 +0800

    assignment week 4

---
+++ Week_04/455.assign-cookies.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=455 lang=java
+ *
+ * [455] Assign Cookies
+ */
+
+// @lc code=start
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) {
+                count++;
+                i++;
+            }
+            j++;
+        }
+        return count;
+    }
+}
+// @lc code=end
